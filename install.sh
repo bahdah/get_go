847f5d8ec6d99305f23865c0f85e070dc469c8cb
Bahdah Shin <bahdahdev@gmail.com>
2022-06-29 11:07:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
 sha256sum go1.18.3.linux-amd64.tar.gz
+# 956f8507b302ab0bb747613695cdae10af99bbd39a90cae522b7c0302cc27245
 sudo tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
 source ~/.profile
